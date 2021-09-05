09629cf50acc2e3478825d5899630a1cf7043208
Techatonic <31274819+Techatonic@users.noreply.github.com>
2021-09-05 22:35:20 +0100

    Completed Exercises 1.2 and 1.3

---
+++ Exercise-1.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = -100;
+    upper = 200;
+    step = 20;
+
+    celsius = lower;
+    while(celsius <= upper){
+        fahr =  celsius*(9.0/5.0) + 32.0;
+        printf("%6.0f %6.0f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+
+}
