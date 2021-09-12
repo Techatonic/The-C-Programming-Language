1bd2c886d46a25ad539c1d7acb4d09ebbc798e5e
Techatonic <31274819+Techatonic@users.noreply.github.com>
2021-09-12 22:30:09 +0100

    Moved to CLion IDE

---
+++ cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.01.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.01.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.02.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.02.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.03.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.03.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.04.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.04.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.05.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.05.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.06.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.06.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.07.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.07.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.08.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.08.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.09.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.09.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.10.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.10.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.12.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.12.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.13.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.13.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.14.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.14.c.o"
+  "/home/danny/CLionProjects/The-C-Programming-Language/Chapter-1/Exercise-1.15.c" "/home/danny/CLionProjects/The-C-Programming-Language/cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.15.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/The_C_Programming_Language.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.01.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.02.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.03.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.04.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.05.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.06.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.07.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.08.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.09.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.10.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.12.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.13.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.14.c.o"
+  "CMakeFiles/The_C_Programming_Language.dir/Chapter-1/Exercise-1.15.c.o"
+  "The_C_Programming_Language"
+  "The_C_Programming_Language.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/The_C_Programming_Language.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
